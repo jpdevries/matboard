6a84a44657788f4041e90402670fff3a86e1799a
JP DeVries <mail@devries.jp>
2016-07-08 14:02:12 +0200

    WIP twig

--- labs/manage-users/server.js
+++ labs/manage-users/server.js
@@ -11,17 +11,15 @@ app.set("twig options", {
     strict_variables: false
 });
 
-app.get('/', function(req, res){
-  //res.render('index.twig');
-  fs.readFile('index.html', function (err, data) {
-    res.writeHead(200, {
-        'Content-Type': 'text/html',
-            'Content-Length': data.length
-    });
-    res.write(data);
-    res.end();
+app.get('/users', function(req, res){
+  getUserRows().then(function(result){
+    res.render('index.twig', result);
+  },function(err){
+    console.log(err);
   });
 });
+
+
 
 
 
@@ -33,11 +31,7 @@ app.get('/api/users',function(req, res){
   });
 });
 
-app.get('/users', function(req, res){
-  res.render('index.twig', {
-    message : "Hello World"
-  });
-});
+
 
 app.post('/add/user', function(req, res){
   processAllFieldsOfTheForm(req,res);
@@ -57,7 +51,7 @@ function getUserRows() {
       if (err) throw err;
 
       // execute a query on our database
-      client.query('SELECT modx_users.username, user_group, role, name, fullname, email, title, active, sudo FROM modx_users INNER JOIN modx_member_groups ON modx_member_groups.member = modx_users.id INNER JOIN modx_membergroup_names ON modx_member_groups.user_group = modx_membergroup_names.id INNER JOIN modx_user_attributes ON modx_user_attributes.id = modx_users.id;', function (err, result) {
+      client.query('SELECT modx_users.username, user_group, role, name as groupname, fullname, email, title, active, sudo FROM modx_users INNER JOIN modx_member_groups ON modx_member_groups.member = modx_users.id INNER JOIN modx_membergroup_names ON modx_member_groups.user_group = modx_membergroup_names.id INNER JOIN modx_user_attributes ON modx_user_attributes.id = modx_users.id;', function (err, result) {
         if (err) throw err;
 
         // disconnect the client
@@ -71,27 +65,34 @@ function getUserRows() {
           row.username
         )))];
 
-        var userGroupNames = {};
+        var userGroupNamesHash = {};
         rows = usernames.map((username) => {
           var filtered = rows.filter((row) => (
             row.username == username
           ));
 
           var userGroups = filtered.map((user) => {
-            if(!userGroupNames[user['user_group']]) userGroupNames[user['user_group']] = user['name'];
+            if(!userGroupNamesHash[user['user_group']]) userGroupNamesHash[user['user_group']] = {id:parseInt(user['user_group']), name:user['groupname']};
             return user.user_group;
           });
 
+
+
           var newRow = filtered[0];
           delete newRow['user_group'];
-          delete newRow['name'];
+          delete newRow['groupname'];
 
           newRow.user_groups = userGroups;
           return newRow;
         });
 
+        var userGroups = [];
+        for(var key in userGroupNamesHash) {
+          userGroups.push(userGroupNamesHash[key]);
+        }
+
         resolve({
-          userGroups:userGroupNames,
+          userGroups:userGroups,
           users:rows
         });
       });
